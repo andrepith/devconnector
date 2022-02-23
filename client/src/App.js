a3efb0777ee9192bab44a9c226a6d52df013ce15
Andre Adikara <andrepith@yahoo.co.uk>
2022-02-23 17:06:42 +0700

    Create initial components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 import "./App.css";
 
 const App = () => (
   <>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </>
 );
 
