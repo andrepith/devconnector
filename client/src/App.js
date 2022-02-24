e8ef58dceac3e4631327eb6cdd1eaf38f1592aa3
Andre Adikara <andrepith@yahoo.co.uk>
2022-02-24 18:07:49 +0700

    Alert component & action call

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import { Provider } from "react-redux";
+import Alert from "./components/layout/Alert";
 import store from "./store";
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = () => (
   <Provider store={store}>
     <Router>
       <Navbar />
+      <Alert />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register />} />
