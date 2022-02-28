a16d50a6f8251e22b837e7e9a9555699877a06b4
Andre Adikara <andrepith@yahoo.co.uk>
2022-02-28 18:47:39 +0700

    Display exp and edu

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 
 const Profile = ({
   getProfileById,
@@ -34,6 +36,29 @@ const Profile = ({
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {!!profile.experience.length ? (
+                profile.experience.map((experience) => (
+                  <ProfileExperience
+                    key={experience._id}
+                    experience={experience}
+                  />
+                ))
+              ) : (
+                <h4>No experience</h4>
+              )}
+            </div>
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {!!profile.education.length ? (
+                profile.education.map((education) => (
+                  <ProfileEducation key={education._id} education={education} />
+                ))
+              ) : (
+                <h4>No education</h4>
+              )}
+            </div>
           </div>
         </>
       )}
