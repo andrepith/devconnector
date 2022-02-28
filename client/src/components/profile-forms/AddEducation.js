6fcbd83f2590fecaccb359266a5ce23133260300
Andre Adikara <andrepith@yahoo.co.uk>
2022-02-28 15:20:18 +0700

    Delete education, experiences & account

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -85,7 +85,7 @@ const AddEducation = ({ addEducation }) => {
                 toggleDisabled(!toDateDisabled);
               }}
             />{" "}
-            Current Job
+            Current School
           </p>
         </div>
         <div className="form-group">
