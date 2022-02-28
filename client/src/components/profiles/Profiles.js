2ac63858fbe205a51041c7b0e96ff4d4e28a2619
Andre Adikara <andrepith@yahoo.co.uk>
2022-02-28 17:26:59 +0700

    Display profiles

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ProfileItem from "./ProfileItem";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+
+  return (
+    <section className="container">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop" /> Browse and connect with
+            developers
+          </p>
+          <div className="profiles">
+            {!!profiles.length ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = ({ profile }) => ({ profile });
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
