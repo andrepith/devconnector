b8d6aa1ebd5c66f7c67b0ac858f3bc3c5ef11b50
Andre Adikara <andrepith@yahoo.co.uk>
2022-03-01 09:41:21 +0700

    Post item component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,12 +3,28 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPosts } from "../../actions/post";
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div>Posts</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <section class="container">
+      <h1 class="large text-primary">Posts</h1>
+      <p class="lead">
+        <i class="fas fa-user"></i> Welcome to the community!
+      </p>
+      {/* PostForm */}
+      <div class="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 Posts.propTypes = {
