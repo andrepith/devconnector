36fb1072471160efefc43edf65e8dbbc499f777c
Andre Adikara <andrepith@yahoo.co.uk>
2022-03-01 10:17:09 +0700

    Adding post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,6 +3,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from "../actions/types";
 
 const initialState = {
@@ -16,6 +17,8 @@ const post = (state = initialState, { type, payload }) => {
   switch (type) {
     case GET_POSTS:
       return { ...state, posts: payload, loading: false };
+    case ADD_POST:
+      return { ...state, posts: [payload, ...state.posts], loading: false };
     case DELETE_POST:
       return {
         ...state,
